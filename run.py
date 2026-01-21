88b33607c771899c05bff2877c16a53c82d30bf3
Reena Arora <72340438+raror-tech@users.noreply.github.com>
2026-01-21 17:08:55 +0530

    Remove db.create_all from production entry point

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 from app import create_app
-from app.extensions import db
 
 
-from app.models import *
+
+
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
+
 
 @app.route("/")
 def home():
